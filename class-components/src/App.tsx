de13d7d997c7574dffd7b32d32e9e8028b2aa709
YourunB <bxyura@gmail.com>
2024-07-06 18:57:26 +0300

    feat: add PageMain component for render cards

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,9 +1,12 @@
 import { Component } from 'react';
 import './App.css';
+import PageMain from './pages/pageMain';
+
 class App extends Component {
   render() {
     return (
       <>
+        <PageMain/>
       </>
     );
   }
---
+++ class-components/src/pages/pageMain.tsx
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import './pageMain.css';
+
+class PageMain extends Component {
+
+  render() {
+
+    return (
+      <div className='page-main'>
+
+        <main className='page-main__main'>
+          <h1>Disney Characters</h1>
+          <section className='cards'>
+            cards box...
+          </section>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default PageMain
