18fad47bca45c4a15d2cc0d293c9e22b88d6e042
YourunB <bxyura@gmail.com>
2024-07-06 19:09:08 +0300

    fead: add component CardCharacter

---
+++ class-components/src/components/cardCharacter.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './cardCharacter.css';
+
+class CardCharacter extends Component {
+  render() {
+    return (
+      <div className='card-char'>
+        Card...
+      </div>
+    );
+  }
+}
+
+export default CardCharacter
