6839138e812d1714e396cf1fa2040ecc06a0e019
YourunB <bxyura@gmail.com>
2024-07-06 19:20:22 +0300

    feat: add load data and show chatacters cards

--- class-components/src/pages/pageMain.tsx
+++ class-components/src/pages/pageMain.tsx
@@ -1,9 +1,45 @@
 import { Component } from 'react';
 import './pageMain.css';
+import { getCharactersPageApi, searchCharactersApi } from '../modules/api';
+import CardCharacter from '../components/cardCharacter';
+import { DisneyObject } from '../state/types';
 
 class PageMain extends Component {
+  state = {
+    search: '',
+    page: 1,
+    obj: {} as DisneyObject,
+  };
+
+  componentDidMount(): void {
+    this.firstCreateCards();
+  }
+
+  async firstCreateCards() {
+    this.setState({
+      obj: await getCharactersPageApi(1, 500),
+    });
+  }
 
   render() {
+    let cardCode = null;
+    if ('data' in this.state.obj) {
+      const data = Array.isArray(this.state.obj.data)
+        ? this.state.obj.data
+        : [this.state.obj.data];
+    
+      cardCode = data.map(v => (
+        <CardCharacter
+          key={v._id}
+          image={v.imageUrl}
+          name={v.name}
+          films={v.films.join(', ')}
+          tvShows={v.tvShows.join(', ')}
+          games={v.videoGames.join(', ')}
+          source={v.sourceUrl}
+        />
+      ));
+    }
 
     return (
       <div className='page-main'>
@@ -11,7 +47,7 @@ class PageMain extends Component {
         <main className='page-main__main'>
           <h1>Disney Characters</h1>
           <section className='cards'>
-            cards box...
+            {cardCode}
           </section>
         </main>
       </div>
---
+++ class-components/src/state/types.tsx
@@ -0,0 +1,35 @@
+export interface DisneyObject {
+  data: [Character] | Character;
+  info: Info;
+}
+
+export interface Character {
+  _id: number;
+  name: string;
+  films: string[];
+  tvShows: string[];
+  videoGames: string[];
+  imageUrl: string;
+  sourceUrl: string;
+}
+
+export interface Info {
+  count: number;
+  nextPage: string;
+  previousPage: string | null
+  totalPages: number;
+}
+
+export interface CardCharacterProps {
+  key: number;
+  image: string;
+  name: string;
+  films: string;
+  tvShows: string;
+  games: string;
+  source: string;
+}
+
+export interface PropsChildren {
+  children: React.ReactNode;
+}
