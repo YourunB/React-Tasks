e37e0de2fab2442c7ce36a02be06f8ade81e5f0c
YourunB <bxyura@gmail.com>
2024-07-06 20:04:23 +0300

    feat: add search and save to localStorage

--- class-components/src/pages/pageMain.tsx
+++ class-components/src/pages/pageMain.tsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import './pageMain.css';
 import { getCharactersPageApi, searchCharactersApi } from '../modules/api';
 import CardCharacter from '../components/cardCharacter';
+import React from 'react';
 import { DisneyObject } from '../state/types';
 
 class PageMain extends Component {
@@ -11,14 +12,61 @@ class PageMain extends Component {
     obj: {} as DisneyObject,
   };
 
+  serchInputRef = React.createRef();
+
   componentDidMount(): void {
     this.firstCreateCards();
   }
 
-  async firstCreateCards() {
-    this.setState({
-      obj: await getCharactersPageApi(1, 500),
-    });
+  changeSearchCharacters() {
+    const input = this.serchInputRef.current as HTMLInputElement;
+    const value = input.value.trim();
+    if (value !== '') {
+      this.setState({
+        search: value,
+      }, this.createCards);
+      this.saveSearchToLocalStoraage(value);
+    }
+  }
+
+  saveSearchToLocalStoraage(value: string) {
+    if (localStorage.searchHistory) {
+      const arr = JSON.parse(localStorage.searchHistory);
+      arr.push(value);
+      localStorage.searchHistory = JSON.stringify(Array.from(new Set(arr)));
+    } else {
+      const arr: string[] = [value];
+      localStorage.searchHistory = JSON.stringify(arr);
+    };
+  }
+
+  firstCreateCards() {
+    if (localStorage.searchHistory) {
+      const arr = JSON.parse(localStorage.searchHistory) as string[];
+      const input = this.serchInputRef.current as HTMLInputElement;
+      input.value = arr[arr.length - 1];
+      this.setState({search: arr[arr.length - 1]}, this.createCards);
+    } else this.createCards();
+  }
+
+  clearSearch() {
+    const input = this.serchInputRef.current as HTMLInputElement;
+    const value = input.value.trim();
+    if (value === '') this.setState({
+      search: '',
+    }, this.createCards);
+  }
+
+  async createCards() {
+    if (this.state.search === '') {
+      this.setState({
+        obj: await getCharactersPageApi(1, 500),
+      });
+    } else {
+      this.setState({
+        obj: await searchCharactersApi(this.state.search, 10),
+      });
+    }
   }
 
   render() {
@@ -43,6 +91,13 @@ class PageMain extends Component {
 
     return (
       <div className='page-main'>
+        
+        <header className='page-main__header'>
+          <div className='search'>
+            <input onInput={() => this.clearSearch()} ref={this.serchInputRef as React.LegacyRef<HTMLInputElement>} className='search__input' placeholder='Search...'></input>
+            <button onClick={() => this.changeSearchCharacters()} className='search__btn'>Search</button>
+          </div>
+        </header>
 
         <main className='page-main__main'>
           <h1>Disney Characters</h1>
