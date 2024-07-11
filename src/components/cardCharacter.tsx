e30823457035d58614aa1f01015d74a485032a45
yourunb <bxyura@gmail.com>
2024-07-11 13:06:22 +0300

    refactor: replace class CardCharacter component with a functional and change project version

--- src/components/cardCharacter.tsx
+++ src/components/cardCharacter.tsx
@@ -1,31 +1,29 @@
-import { Component } from 'react';
 import './cardCharacter.css';
 import { CardCharacterProps } from '../state/types';
 
-class CardCharacter extends Component<CardCharacterProps> {
-  render() {
-    return (
-      <a className="card-char" href={this.props.source} target="_blank" rel="noreferrer">
-        <img
-          className="card-char__img"
-          src={this.props.image || 'https://github.com/YourunB/Test1/blob/main/images/noimage.jpg?raw=true'}
-        ></img>
-        <h4 className="card-char__title">{this.props.name}</h4>
-        <p className="card-char__description">
-          <span>Films: </span>
-          {this.props.films || 'none'}
-        </p>
-        <p className="card-char__description">
-          <span>TV Shows: </span>
-          {this.props.tvShows || 'none'}
-        </p>
-        <p className="card-char__description">
-          <span>Games: </span>
-          {this.props.games || 'none'}
-        </p>
-      </a>
-    );
-  }
-}
+const CardCharacter = (props: CardCharacterProps) => {
+  return (
+    <a className="card-char" href={props.source} target="_blank" rel="noreferrer">
+      <img
+        className="card-char__img"
+        src={props.image || 'https://github.com/YourunB/Test1/blob/main/images/noimage.jpg?raw=true'}
+        alt={props.name}
+      />
+      <h4 className="card-char__title">{props.name}</h4>
+      <p className="card-char__description">
+        <span>Films: </span>
+        {props.films || 'none'}
+      </p>
+      <p className="card-char__description">
+        <span>TV Shows: </span>
+        {props.tvShows || 'none'}
+      </p>
+      <p className="card-char__description">
+        <span>Games: </span>
+        {props.games || 'none'}
+      </p>
+    </a>
+  );
+};
 
 export default CardCharacter;
