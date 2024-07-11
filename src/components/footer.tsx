f1abd5255891ac93caba6e351c991c9e9e3d3600
yourunb <bxyura@gmail.com>
2024-07-11 13:24:40 +0300

    refactor: replace class Footer component with a functional

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,14 @@
-import { Component } from 'react';
 import './footer.css';
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer">
-        <span>&copy; 2024</span>
-        <a className="footer__link" href="https://github.com/YourunB" target="_blank" rel="noreferrer">
-          Yury Butskevich
-        </a>
-      </footer>
-    );
-  }
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <span>&copy; 2024</span>
+      <a className="footer__link" href="https://github.com/YourunB" target="_blank" rel="noreferrer">
+        Yury Butskevich
+      </a>
+    </footer>
+  );
 }
 
 export default Footer;
