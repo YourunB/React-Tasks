a8289d297f03fcc92c74e40d3a0b8abb0967ebe6
yourunb <bxyura@gmail.com>
2024-07-13 20:40:20 +0300

    feat: add msg for nothing found in CardList

--- src/components/cardList.tsx
+++ src/components/cardList.tsx
@@ -2,13 +2,14 @@ import './cardList.css';
 import { CardListProps } from '../state/types';
 
 const CardList = (props: CardListProps) => {
+  console.log(props.cardCode)
   return (
     <main className="card-list">
       <button className="btn-error" onClick={() => props.createError()}>
         Error
       </button>
       <h1>Disney Characters</h1>
-      <section className="cards">{props.cardCode as JSX.Element}</section>
+      <section className="cards">{Array.isArray(props.cardCode) && props.cardCode.length > 0 ? props.cardCode : 'Nothing found...'}</section>
     </main>
   );
 }
