a9428eb012828880b0505e2795e4f8a71257223e
yourunb <bxyura@gmail.com>
2024-07-14 16:12:51 +0300

    feat: add test for cardList component

---
+++ test/components/cardList.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { vi, expect } from "vitest";
+import '@testing-library/jest-dom';
+import React from "react";
+import CardList from '../../src/components/cardList';
+import { CardListProps } from '../../src/state/types';
+
+describe('CardList Component', () => {
+  const props: CardListProps = {
+    key: 3001,
+    cardCode: [],
+    createError: vi.fn(),
+  };
+  test('renders the title', () => {
+    render(<CardList {...props} />);
+    expect(screen.getByText('Disney Characters')).toBeInTheDocument();
+  });
+
+  test('renders "Nothing found..." when cardCode is empty', () => {
+    render(<CardList {...props} />);
+    expect(screen.getByText('Nothing found...')).toBeInTheDocument();
+  });
+
+  test('calls createError when the error button is clicked', () => {
+    render(<CardList {...props} />);
+    fireEvent.click(screen.getByText('Error'));
+    expect(props.createError).toHaveBeenCalled();
+  });
+});
