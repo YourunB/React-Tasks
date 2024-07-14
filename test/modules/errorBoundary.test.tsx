e68915984a2e03a7156e1b322b9f76796908fbe2
yourunb <bxyura@gmail.com>
2024-07-14 17:11:13 +0300

    refactort: delete PropsChildren from ErrorBoundary test

--- test/modules/errorBoundary.test.tsx
+++ test/modules/errorBoundary.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import ErrorBoundary from '../../src/modules/errorBoundary';
-import { PropsChildren } from '../../src/state/types';
 import React from "react";
 import '@testing-library/jest-dom';
 
