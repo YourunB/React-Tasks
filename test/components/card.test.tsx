0644ee3df145a6fb3f97c5d56d3af5d6bcdf4277
yourunb <bxyura@gmail.com>
2024-07-14 14:42:14 +0300

    feat: add vitest and test for Card component

---
+++ test/components/card.test.tsx
@@ -0,0 +1,26 @@
+import { vi, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Card from '../../src/components/card';
+import '@testing-library/jest-dom';
+import React from "react";
+
+describe('check Card', () => {
+  test('renders Card component with props', () => {
+    const props = {
+      key: 20,
+      id: 20,
+      name: 'Elena of Avalor',
+      image: 'https://static.wikia.nocookie.net/disney/images/f/fa/Normal_EoA_S3_E4_0217.jpg',
+      films: '',
+      showDescription: vi.fn(),
+      };
+
+    render(<Card {...props} />);
+
+    const nameElement = screen.getByText('Elena of Avalor');
+    const filmsElement = screen.getByText('none');
+
+    expect(nameElement).toBeInTheDocument();
+    expect(filmsElement).toBeInTheDocument();
+  });
+});
