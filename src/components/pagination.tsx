903d4508070a7e397a9b8f2eb544e3e3f3663f9c
yourunb <bxyura@gmail.com>
2024-07-21 14:14:42 +0300

    refactor: replace Link in pagination by useNavigate for change page

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,20 +1,19 @@
 import './pagination.css';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 
 const Pagination = () => {
+  const navigate = useNavigate();
   const dataReduxPage = useSelector( (state: RootState) => state.dataPage );
+
+  const changePage = (value: number) => navigate(`/${Number(dataReduxPage.page) + value}/${dataReduxPage.search}`);
   
   return (
     <div className="pagination">
-      <button className="pagination__btn" disabled={dataReduxPage.page > 1 ? false : true}>
-        <Link to={`/${dataReduxPage.page - 1}/${dataReduxPage.search}`}>&#60;</Link>
-      </button>
+      <button className="pagination__btn" disabled={dataReduxPage.page > 1 ? false : true} onClick={() => changePage(-1)}>&#60;</button>
       <span className="pagination__count">{dataReduxPage.page}</span>
-      <button className="pagination__btn" disabled={dataReduxPage.page < dataReduxPage.totalPages ? false : true}>
-        <Link to={`/${Number(dataReduxPage.page) + 1}/${dataReduxPage.search}`}> &#62;</Link>
-      </button>
+      <button className="pagination__btn" disabled={dataReduxPage.page < dataReduxPage.totalPages ? false : true} onClick={() => changePage(1)}>&#62;</button>
     </div>
   );
 };
