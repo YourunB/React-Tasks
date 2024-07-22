36a0aa4b5a449755adf2870eebb33eace77cf95e
yourunb <bxyura@gmail.com>
2024-07-22 10:44:42 +0300

    refactor: delete all props in Search and Search work with Redux

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,19 +1,51 @@
 import './search.css';
 import { SearchProps } from '../state/types';
+import { useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
+import { RootState } from '../redux/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { updatePage, updateSearch } from '../redux/dataSlicePage';
 
-const Search = (props: SearchProps) => {
+const Search = () => {
+  const navigate = useNavigate();
+  const serchInputRef = useRef(null);
+  const dataReduxPage = useSelector( (state: RootState) => state.dataPage );
+  const dispatch = useDispatch();
+
+  function clearSearch() {
+    const input = serchInputRef.current as HTMLInputElement | null;
+    if (input) input.value = '';
+    if (dataReduxPage.search) {
+      dispatch(updatePage(1));
+      dispatch(updateSearch(''));
+      navigate(`/${1}/${''}`);
+    }
+  }
+
+  function changeSearchCharacters() {
+    if (serchInputRef.current) {
+      const input = serchInputRef.current as HTMLInputElement;
+      const value = input.value.trim();
+      if (value !== '') {
+        dispatch(updatePage(1));
+        dispatch(updateSearch(value));
+        navigate(`/${1}/${value}`);
+      }
+    }
+  }
+
   return (
     <div className="search" data-testid={'search'}>
-      <button onClick={() => props.clearSearch()} className="search__btn-clear">
+      <button onClick={() => clearSearch()} className="search__btn-clear">
         X
       </button>
       <input
-        ref={props.serchInputRef as React.LegacyRef<HTMLInputElement>}
+        ref={serchInputRef}
         className="search__input"
         placeholder="Search..."
-        defaultValue={props.search}
+        defaultValue={dataReduxPage.search}
       />
-      <button onClick={() => props.changeSearchCharacters()} className="search__btn">
+      <button onClick={() => changeSearchCharacters()} className="search__btn">
         Search
       </button>
     </div>
