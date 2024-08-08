6200a1bf300571c3a959d2cbb4775e0c4fbb2e1d
yourunb <bxyura@gmail.com>
2024-08-08 19:39:03 +0300

    feat: add theme to next

--- disney-characters/src/pages/_app.tsx
+++ disney-characters/src/pages/_app.tsx
@@ -2,11 +2,21 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from 'react-redux';
 import { store } from "@/redux/store";
+import ThemeContext from "@/components/themeContext";
+
+const theme = {
+  light: false,
+  change: function () {
+    this.light = !this.light;
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ThemeContext.Provider value={theme}>
+        <Component {...pageProps} />
+      </ThemeContext.Provider>
     </Provider>
   )
 }
