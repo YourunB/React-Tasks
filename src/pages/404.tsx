9cc36f9b2cb092296f87363b89c29ade20b9f16d
yourunb <bxyura@gmail.com>
2024-08-09 22:33:11 +0300

    feat: add tests for 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,9 @@
 import Head from 'next/head';
 import s from '../../src/styles/pageNotFound.module.css'
 import earthImg from '../../public/earth.svg';
-import ThemeContext from '../components/themeContext';
-import { useContext } from 'react';
-import Image from '../../node_modules/next/image';
 import Link from '../../node_modules/next/link';
 
 export default function PageNotFound() {
-  const theme = useContext(ThemeContext);
-  
   return (
     <>
       <Head>
@@ -19,7 +14,7 @@ export default function PageNotFound() {
       </Head>
       <main className={`${s['page-not-found']}`} data-testid='page-not-found'>
         <div className={s['error-box']}>
-          <Image className={s['error-box__img']} src={earthImg} alt="Earth" />
+          <img className={s['error-box__img']} src={earthImg} alt="Earth" />
           <h2 className={s['error-box__title']}>404</h2>
           <p className={s['error-box__description']}>This Page Not Found</p>
           <Link className={s['error-box__link']} href="/">
