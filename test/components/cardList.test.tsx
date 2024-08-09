f9df1c9513a92aa385eef6d5e02b74f26a7ee810
yourunb <bxyura@gmail.com>
2024-08-09 22:47:04 +0300

    feat: add test fot CardList component

---
+++ test/components/cardList.test.tsx
@@ -0,0 +1,23 @@
+import { describe, test, expect } from 'vitest';
+import { render } from '@testing-library/react';
+import CardList from '../../src/components/cardList';
+import { CardListProps } from '../../src/state/types';
+import React from 'react';
+import '@testing-library/jest-dom';
+
+describe('CardList Component', () => {
+  test('renders "Nothing found..." when cardCode is not an array or is empty', () => {
+    const props: CardListProps = { key: 3002, cardCode: [] };
+    const { getByText } = render(<CardList {...props} />);
+    expect(getByText('Nothing found...')).toBeInTheDocument();
+  });
+
+  test('renders cardCode elements when cardCode is a non-empty array', () => {
+    const CardOne = <div>Mickey Mouse</div>;
+    const CardTwo = <div>Donald Duck</div>;
+    const props: CardListProps = { key: 3002, cardCode: [CardOne, CardTwo] };
+    const { getByText } = render(<CardList {...props} />);
+    expect(getByText('Mickey Mouse')).toBeInTheDocument();
+    expect(getByText('Donald Duck')).toBeInTheDocument();
+  });
+});
