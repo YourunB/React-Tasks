9646a784d447c1e759d07132ffd8e8ff8a5b98f2
yourunb <bxyura@gmail.com>
2024-08-09 21:58:04 +0300

    feat: add tests for Card component

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     globals: true,
     coverage: {
       include: ['src/**/*.ts', 'src/**/*.tsx'],
-      exclude: ['src/**/excluded-folder/**', 'src/main.tsx'],
+      //exclude: ['src/**/excluded-folder/**'],
     },
   },
 });
