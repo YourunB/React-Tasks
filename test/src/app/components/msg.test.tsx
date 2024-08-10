2d4e55ee10f818d1eb9798090590d8f81c6e3034
yourunb <bxyura@gmail.com>
2024-08-10 15:18:05 +0300

    feat: add test for components

---
+++ test/src/app/components/msg.test.tsx
@@ -0,0 +1,48 @@
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import React from 'react';
+import Msg from '../../../../src/app/components/msg';
+import { vi, test, describe, expect } from 'vitest';
+
+beforeAll(() => {
+  global.URL.createObjectURL = vi.fn();
+});
+
+const mockStore = configureStore([]);
+const initialState = {
+  dataElements: {
+    checkedCards: [
+      { id: 0, name: '' },
+      { id: 1, name: 'Name 1' },
+      { id: 2, name: 'Name 2' },
+      { id: 3, name: 'Name 3' },
+    ],
+  },
+};
+
+describe('Msg Component', () => {
+  const store = mockStore(initialState);
+  const renderComponent = () => {
+    return render(
+      <Provider store={store}>
+        <Msg />
+      </Provider>
+    );
+  };
+
+  test('should display the correct number of selected items', () => {
+    renderComponent();
+
+    expect(screen.getByText('Total selected: 3')).toBeInTheDocument();
+  });
+
+  test('should call clearAll when Reset button is clicked', () => {
+    renderComponent();
+
+    const btn = screen.getByText('Reset');
+    expect(btn).toBeInTheDocument();
+    fireEvent.click(btn);
+  });
+});
---
+++ test/src/app/components/theme.test.tsx
@@ -0,0 +1,21 @@
+import { renderHook } from '@testing-library/react';
+import { act } from 'react';
+import { useContext } from 'react';
+import ThemeContext from '../../../../src/app/components/themeContext';
+import { describe, test, expect } from 'vitest'; 
+
+describe('ThemeContext', () => {
+  test('initial light value as false', () => {
+    const { result } = renderHook(() => useContext(ThemeContext));
+    expect(result.current.light).toBe(false);
+  });
+
+  test('toggle light', () => {
+    const { result } = renderHook(() => useContext(ThemeContext));
+
+    act(() => result.current.change());
+    expect(result.current.light).toBe(true);
+    act(() => result.current.change());
+    expect(result.current.light).toBe(false);
+  });
+});
