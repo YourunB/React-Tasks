b5af7d36f04a681a8b2f94fc13924544667b8a3e
yourunb <bxyura@gmail.com>
2024-08-11 09:30:31 +0300

    feat: create remix project

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    plugins: [remix()],
+  },
 };
 
 export default nextConfig;
