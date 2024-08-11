a8a48706e03e2927300d3e558dc3bc21ab982338
yourunb <bxyura@gmail.com>
2024-08-11 15:06:18 +0300

    refactor: change config and add husky and prettier

--- rick-morty/.eslintrc.cjs
+++ rick-morty/.eslintrc.cjs
@@ -1,9 +1,3 @@
-/**
- * This is intended to be a basic starting point for linting in your app.
- * It relies on recommended configs out of the box for simplicity, but you can
- * and should modify this configuration to best suit your team's needs.
- */
-
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
@@ -21,11 +15,9 @@ module.exports = {
   },
   ignorePatterns: ["!**/.server", "!**/.client"],
 
-  // Base config
   extends: ["eslint:recommended"],
 
   overrides: [
-    // React
     {
       files: ["**/*.{js,jsx,ts,tsx}"],
       plugins: ["react", "jsx-a11y"],
@@ -50,7 +42,6 @@ module.exports = {
       },
     },
 
-    // Typescript
     {
       files: ["**/*.{ts,tsx}"],
       plugins: ["@typescript-eslint", "import"],
@@ -73,7 +64,6 @@ module.exports = {
       ],
     },
 
-    // Node
     {
       files: [".eslintrc.cjs"],
       env: {
