d37d78a4cbbf94cf424de2ea566f622c1abab765
yourunb <bxyura@gmail.com>
2024-08-11 12:03:14 +0300

    feat: add redux and theme to app

--- rick-morty/app/root.tsx
+++ rick-morty/app/root.tsx
@@ -6,22 +6,36 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./index.css";
+import { store } from "./redux/store";
+import { Provider } from 'react-redux';
+import ThemeContext from './components/themeContext';
+
+const theme = {
+  light: false,
+  change: function () {
+    this.light = !this.light;
+  },
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <Provider store={store}>
+      <ThemeContext.Provider value={theme}>
+        <html lang="en">
+          <head>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <Meta />
+            <Links />
+          </head>
+          <body>
+            {children}
+            <ScrollRestoration />
+            <Scripts />
+          </body>
+        </html>
+      </ThemeContext.Provider>
+    </Provider>
   );
 }
 
