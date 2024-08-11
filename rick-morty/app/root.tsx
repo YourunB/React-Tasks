1f8eb512ad565ebdfc969b760178a51374524920
yourunb <bxyura@gmail.com>
2024-08-11 12:05:23 +0300

    feat: add favicon and images for app

--- rick-morty/app/root.tsx
+++ rick-morty/app/root.tsx
@@ -25,6 +25,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
           <head>
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
             <Meta />
             <Links />
           </head>
