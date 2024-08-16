8737fa293c0f11530121cfa0e61cda9738dc5d8d
yourunb <bxyura@gmail.com>
2024-08-16 16:47:23 +0300

    feat: add Link to header and add style for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
+import s from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
-    <header>
-      <ul>
-        <li>Main</li>
-        <li>Uncontrolled Form</li>
-        <li>React Hook Form</li>
+    <header className={s.header}>
+      <ul className={s.menu}>
+        <li>
+          <Link to='/'>Main</Link>
+        </li>
+        <li>
+          <Link to='/form1'>Uncontrolled Form</Link>
+        </li>
+        <li>
+          <Link to='/form1'>React Hook Form</Link>
+        </li>
       </ul>
     </header>
   );
