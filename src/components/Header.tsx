cc916e0bbf9fa1dc0d7e0c917151cccef9e3fc8d
yourunb <bxyura@gmail.com>
2024-08-17 16:11:43 +0300

    feat: add checked current menu item

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,23 @@
 import s from './Header.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const Header = () => {
+  const location = useLocation();
+
+  const isCurrentPage = (itemPath) => {
+    return itemPath === location.pathname;
+  };
+
   return (
     <header className={s.header}>
       <ul className={s.menu}>
-        <li>
+        <li className={`${s['menu__item']} ${isCurrentPage('/') ? s['menu__item_checked'] : ''}`}>
           <Link to='/'>Main</Link>
         </li>
-        <li>
+        <li className={`${s['menu__item']} ${isCurrentPage('/uncontrolled') ? s['menu__item_checked'] : ''}`}>
           <Link to='/uncontrolled'>Uncontrolled Form</Link>
         </li>
-        <li>
+        <li className={`${s['menu__item']} ${isCurrentPage('/controlled') ? s['menu__item_checked'] : ''}`}>
           <Link to='/controlled'>React Hook Form</Link>
         </li>
       </ul>
