cde8ff8eac5b4440fa07fbbd4e7a80c8adbff8f7
yourunb <bxyura@gmail.com>
2024-08-17 16:49:12 +0300

    feat: add link to page not found and change style, and add earth image

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,8 +1,16 @@
 
+import earthImg from '../assets/images/earth.svg';
+import { Link } from 'react-router-dom';
+
 export const PageNotFound = () => {
   return (
-    <main>
-      Page not found ...
+    <main className="page not-found">
+      <div className='not-found__box'>
+        <img src={earthImg} alt='Earth'/>
+        <h3>404</h3>
+        <p>Page Not Found</p>
+        <Link className='not-found__box__link' to='/'>Home</Link>
+      </div>
     </main>
   );
 };
